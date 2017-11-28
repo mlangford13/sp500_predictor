e93c447c32342ced7e3ecc65d65d3cae8011fb24
Bryan Gardner <gardner.bryan22@gmail.com>
2017-11-28 11:48:35 -0500

    lag data by one day

--- sp_downloader.R
+++ sp_downloader.R
@@ -1,84 +1,120 @@
-#install.packages("rvest")
-#install.packages("xts")
+#install.packages("neuralnet")
 #install.packages("Quandl")
 
-library(xml2)
-library(rvest)
-library(xts)
+rm(list=ls())
+
 library(Quandl)
 library(neuralnet)
 
+start_date <- as.Date("2010-01-01")
 today <- Sys.Date()
 
-dates <- seq.Date(from=as.Date("1990-01-01"), to=Sys.Date(), by=1)
-View(dates)
+dates <- seq.Date(from=as.Date("2010-01-01"), to=Sys.Date(), by=1)
+head(dates)
 
+# generic wrapper to get stock data
 getStockData <- function(ticker) {
-  return(Quandl.datatable("WIKI/PRICES", ticker=c(ticker), date.gte="1990-01-01", date.lte=Sys.Date(), paginate=TRUE, qopts.columns=c("ticker", "date", "close")))
+  return(Quandl.datatable("WIKI/PRICES",
+                          ticker=c(ticker),
+                          date.gte="2010-01-01",
+                          date.lte=Sys.Date(),
+                          paginate=TRUE,
+                          qopts.columns=c("Date","ticker", "close")
+                          )
+         )
 }
 
+# normalization function
 mmnorm <- function(x) {
   return((x - min(x)) / (max(x) - min(x)))
 }
 
+# denormalization function
 denorm <- function(x, min, max) {
   return(x * (max - min) + min)
 }
 
-
-gold_prices <- Quandl("WGC/GOLD_DAILY_USD", date.gte="1990-01-01", date.lte=today)
+# get gold prices
+gold_prices <- Quandl("WGC/GOLD_DAILY_USD", start_date="2010-01-01", end_date=today)
 View(gold_prices)
 
-treasury_yield <- Quandl("USTREASURY/YIELD", date.gte="1990-01-01", date.lte=today)
+# get treasury yield
+treasury_yield <- Quandl("USTREASURY/YIELD", start_date="2010-01-01", end_date=today)
 View(treasury_yield)
 
+# get stock pricse
 exxon <- getStockData("XOM")
-View(exxon)
-
-apple <- getStockData("AAPL")
+ge <- getStockData("GE")
 jpm <- getStockData("JPM")
 
+# get oil prices
+oil <-Quandl("OPEC/ORB", date.gte="2010-01-01", date.lte=today)
+View(oil)
+
+# read in SP data
 sp_data = read.csv("GSPC.csv")
-sp_data[,1] <- as.Date(sp_data[,1])
-typeof(sp_data[,4])
+sp_data[,1] <- as.Date(sp_data[,1], format="%m/%d/%Y")
 View(sp_data)
 
+# Add the next day S&P close to the current day
+time_series_sp_data<-NULL
+for (s in seq(from=1, to=nrow(sp_data)-1, by=1)) {
+  time_series_sp_data<-rbind(time_series_sp_data, cbind(sp_data[s,], Next_Close=sp_data[s+1,5]))
+}
+View(time_series_sp_data)
 
+# Join all the datasets on date
 joined_data <- NULL
 for (d in as.list(dates)) {
    gold_ind <- which(gold_prices[,1] == d, arr.ind=TRUE)
-   exxon_ind <- which(exxon[,2] == d, arr.ind=TRUE)
-   apple_ind <- which(apple[,2] == d, arr.ind=TRUE)
-   jpm_ind <- which(jpm[,2] == d, arr.ind=TRUE)
-   sp_ind <- which(sp_data[,1] == d, arr.ind=TRUE)
+   exxon_ind <- which(exxon[,1] == d, arr.ind=TRUE)
+   ge_ind <- which(ge[,1] == d, arr.ind=TRUE)
+   jpm_ind <- which(jpm[,1] == d, arr.ind=TRUE)
+   sp_ind <- which(time_series_sp_data[,1] == d, arr.ind=TRUE)
    treasury_ind <- which(treasury_yield[,1] == d, arr.ind=TRUE)
+   oil_ind <- which(oil[,1] == d, arr.ind=TRUE)
    
-   if (length(gold_ind) > 0 && length(exxon_ind) > 0 && length(apple_ind) > 0 && length(google_ind) > 0 && length(sp_ind) > 0 && length(treasury_ind)) {
+   if (length(gold_ind) > 0 && length(exxon_ind) > 0 && length(ge_ind) > 0 && length(jpm_ind) > 0 && length(sp_ind) > 0 && length(treasury_ind) > 0 && length(oil_ind) > 0) {
      joined_data<-rbind(joined_data,
                         data.frame(
                           Date=as.Date(d),
                           Gold_Close=gold_prices[gold_ind, 2],
                           XOM=exxon[exxon_ind, 3],
-                          AAPL=apple[apple_ind, 3],
-                          GOOG=google[google_ind, 3],
+                          GE=ge[ge_ind, 3],
+                          JPM=jpm[jpm_ind, 3],
                           TEN_YEAR=treasury_yield[treasury_ind, 10],
-                          SP=sp_data[sp_ind, 5]))
+                          ONE_YEAR=treasury_yield[treasury_ind, 5],
+                          THREE_YEAR=treasury_yield[treasury_ind, 7],
+                          OIL=oil[oil_ind, 2],
+                          SP_CLOSE=time_series_sp_data[sp_ind, 5],
+                          NEXT_SP=time_series_sp_data[sp_ind, 8]))
    }
 }
+View(joined_data)
 
-min <- min(joined_data$SP)
-max <- max(joined_data$SP)
+min <- min(joined_data$NEXT_SP)
+max <- max(joined_data$NEXT_SP)
 
-normalized<-as.data.frame(lapply(joined_data[,-1], mmnorm))
+normalized<-as.data.frame(lapply(joined_data[,!names(joined_data) %in% c("Date")], mmnorm))
+View(normalized)
+
 ind <- seq(from=1, to=nrow(normalized), by=5)
 test <- normalized[ind,]
 training <- normalized[-ind,]
 View(normalized)
 
-nn<-neuralnet(SP~Gold_Close+XOM+AAPL+GOOG+TEN_YEAR, data=training, hidden=c(10))
+View(training)
+nn<-neuralnet(NEXT_SP~Gold_Close+XOM+GE+JPM+TEN_YEAR+ONE_YEAR+THREE_YEAR+OIL+SP_CLOSE, data=training, hidden=c(5,5))
 plot(nn)
 
-results <- compute(nn, test[,-6])$net.result
+head(test)
+results <- compute(nn, test[,!names(test) %in% c("NEXT_SP")])$net.result
 denorm_results <- denorm(results, min, max)
 
-View(cbind(joined_data[-ind, 7], denorm_results))
+pred <- cbind(joined_data[ind,], PREDICTED_NEXT_SP=denorm_results)
+View(pred)
+
+correct_moves <- sum((pred$SP_CLOSE < pred$NEXT_SP & pred$SP_CLOSE < pred$PREDICTED_NEXT_SP) | 
+      (pred$SP_CLOSE > pred$NEXT_SP & pred$SP_CLOSE > pred$PREDICTED_NEXT_SP))
+
+correct_moves/nrow(pred)
